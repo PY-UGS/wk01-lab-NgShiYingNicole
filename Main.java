d1a52c518f64711fbe94ae35c81bb88bf5a0147e
NgShiYingNicole <97488614+NgShiYingNicole@users.noreply.github.com>
2022-01-11 15:40:00 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello, I am Ng Shi Ying Nicole!"); 
+
+		String module = "CSC1009"; 
+		switch(module) { 
+		case "CSC1006":
+			System.out.println("Mathematics 2"); break; 
+		case "CSC1007":
+			System.out.println("Operating Systems"); 
+			break; 
+		case "CSC1008":
+			System.out.println("Data Structures and Algorithms"); 
+			break;
+		case "CSC1009":
+			System.out.println("Object Oriented Programming"); 
+			break;
+		case "CSC1010":
+			System.out.println("Computer Networks");
+			break; 
+		default:
+			System.out.println("Unknown module"); 
+			break; 
+		}
+		System.out.println("After switch");
+
+		for(int x = 102; x>65;x--){ 
+			if(x%2 != 0){
+				System.out.println("value of x : "+x); 
+				System.out.println("\n"); 
+			} 
+		}
+
+	}
+}
